473b1ded538db3c6fab35257aebae7825c72057c
Konstantin Mikov <1konstantinmikov@gmail.com>
2022-08-25 16:35:28 +0300

    sounds howler

--- scripts/sfx.js
+++ scripts/sfx.js
@@ -1,19 +1,19 @@
 export default class Sfx {
   constructor() {
-    this.sound = new Howl({
-      src: ['./sfx/arrowSound.mp3']
-    });
+    this.arrowSound = new Howl({ src: ['./sfx/arrowSound.mp3'] });
+    this.snakeEatSound = new Howl({ src: ['./sfx/eat.mp3'] });
+    this.gameoverSound = new Howl({ src: ['./sfx/gameover.mp3'] });
+    this.loselifeSound = new Howl({ src: ['./sfx/ohmy.mp3'] });
+    this.lvlupSound = new Howl({ src: ['./sfx/lvlup.mp3'] });
+    this.damageSound = new Howl({ src: ['./sfx/damage.mp3'] });
+    this.easterSound = new Howl({ src: ['./sfx/easter.mp3'] });
   }
 
-  arrowPress() { 
-    this.sound.play();
-  }
-
-
-  snakeEat() { new Audio('./sfx/eat.mp3').play() }
-  gameover() { new Audio('./sfx/gameover.mp3').play() }
-  loselife() { new Audio('./sfx/ohmy.mp3').play() }
-  lvlup() { new Audio('./sfx/lvlup.mp3').play() }
-  damage() { new Audio('./sfx/damage.mp3').play() }
-  easter() { new Audio('./sfx/easter.mp3').play() }
+  arrowPress() { this.arrowSound.play() }
+  snakeEat() { this.snakeEatSound.play() }
+  gameover() { this.gameoverSound.play() }
+  loselife() { this.loselifeSound.play() }
+  lvlup() { this.lvlupSound.play() }
+  damage() { this.damageSound.play() }
+  easter() { this.easterSound.play() }
 }
